e43afda2ef5125bb10079a196ff5aa824ce46ef8
Abraham Araujo Moreno <alex_91y@hotmail.com>
2016-11-23 23:35:25 -0600

    Add files via upload

---
+++ src/cicloFOR/Ciclo.java
@@ -0,0 +1,15 @@
+package cicloFOR;
+
+public class Ciclo {
+	
+	public static void main(String []args)
+	{
+		int numero ;
+		
+		for(numero = 0; numero <= 100; numero++)
+		{
+			System.out.println("numero = " + numero);
+		}
+	}
+
+}
